09116c03f845a56097cbb0f12174a97a76bb90b1
JyotiBhambhu <jyotic26@gmail.com>
2023-06-25 20:23:44 +0530

    Bmi Calculator for iOS with StoryBoard

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("BMI calculation scence loaded")
+        bmiLbl.isHidden = true
     }
+   
+    @IBOutlet weak var bmiLbl: UILabel!
+    @IBOutlet weak var weightTxt: UITextField!
+    @IBOutlet weak var heightTxt: UITextField!
+    
+    @IBAction func calculateBtn(_ sender: Any) {
+        let weight = Float(weightTxt.text!)!
+        let height = Float(heightTxt.text!)!
+        let bmi = Double((weight / height / height) * 10000).rounded(toPlaces: 1)
+        print("bmi", bmi, "weight", weight, "height", height)
+        bmiLbl.text = "Your BMI is: \(String(bmi))"
+        bmiLbl.isHidden = false
+    }
+}
 
-
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
 }
 
